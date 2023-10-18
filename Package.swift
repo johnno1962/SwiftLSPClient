d0a6ffa69fa12de9e5fe5abba2ceca83296ed5e6
John Holdsworth <>
2023-10-18 15:33:28 +0200

    Update dependendancies

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftLSPClient",
-    platforms: [.macOS("10.10")],
+    platforms: [.macOS("10.15")],
     products: [
         .library(name: "SwiftLSPClient", targets: ["SwiftLSPClient"]),
     ],
     dependencies: [
         .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.0"),
-        .package(url: "https://github.com/ChimeHQ/JSONRPC", from: "0.2.1"),
+        .package(url: "https://github.com/ChimeHQ/JSONRPC", .upToNextMinor(from: "0.2.1")),
     ],
     targets: [
         .target(name: "SwiftLSPClient", dependencies: ["JSONRPC", "AnyCodable"], path: "SwiftLSPClient/"),
